0bd571061bbf7b95cd06e18139e5fcc885b8eda9
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-05-27 11:32:33 -0400

    delete works

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('cheese');
+INSERT INTO burgers (name) VALUES ('Black Bean');
+INSERT INTO burgers (name) VALUES ('rodeo');
+INSERT INTO burgers (name) VALUES ('turkey');
+INSERT INTO burgers (name) VALUES ('spicy');
