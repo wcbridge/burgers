521820354af5b775260a1b29cbf709c74af4bc92
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-05-28 17:14:35 -0400

    npm update

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+var PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
