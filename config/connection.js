2cb181d017613ea1538c55663b936ca9e8901f3d
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-06-04 11:17:51 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Happyboy1!",
-  database: "burger"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  //Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+  connection = mysql.createConnection({
+    root: 3000,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burger",
+  });
+};
+
 
 // Make connection.
 connection.connect(function(err) {
